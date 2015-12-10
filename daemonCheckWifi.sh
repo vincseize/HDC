59999adad6025ab64cdbd53306f0a7584a42f703
vincseize <vincseize@gmail.com>
2015-12-10 23:30:01 +0100

    Update daemonCheckWifi.sh

--- daemonCheckWifi.sh
+++ daemonCheckWifi.sh
@@ -2,11 +2,17 @@
 
 while true  
         do
-	echo $(date)" Check Wifi"
-        #echo $(date)
-	#iwlist wlan0 scan
-	nmcli  -f NAME con status
+	# echo $(date)" Check Wifi"
+
+	echo "######## if wifi connection and give name ################"
+	iwconfig 2>&1 | grep ESSID
+	#echo "######### all wifi ###########"
+	#iwlist wlan0 scan # all wifi
+	echo "######################"
+	nmcli -f NAME con status # --> NOM : NAME CONNECTION
+	#echo "######## scan wifi name if authorized     #############"
 	#iwlist NEUF_D918 scan # verboten sometimes
-	nmcli con list id "NEUF_D918" | awk '/key-mgmt/ {print $2}' # give wpa or other
+	echo "## Details even not connected ####"
+	nmcli con list id "NEUF_D918" | awk '/key-mgmt/ {print $2}' # ---> TYPE WPA
         sleep 5
 done  
